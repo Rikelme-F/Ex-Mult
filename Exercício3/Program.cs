d1cae529001b535a064e4404b8b52712da66a6c6
Rikelme-F <Rikelme.fsantos@senacsp.edu.br>
2025-03-20 21:45:45 -0300

    Adicionar arquivos de projeto.

---
+++ Exercício3/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Exercício3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escreva um número");
+            string numero1 = Console.ReadLine();
+
+            Console.WriteLine("Escreva mais um número");
+            string numero2 = Console.ReadLine();
+
+            double num1 = Convert.ToDouble(numero1);
+            double num2 = Convert.ToDouble(numero2);
+
+
+            double soma = num1 * num2;
+            Console.WriteLine("A soma dos números é: " + soma);
+        }
+    }
+}
